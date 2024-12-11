8ab79c1378e989d78cabb56f7f184f1413c587fd
Matthew Aguiar <mattaguiar255@protonmail.ch>
2024-12-10 22:24:27 -0500

    Update install-partdb.sh to Output Initial Password

--- scripts/install-partdb.sh
+++ scripts/install-partdb.sh
@@ -20,13 +20,12 @@ set -euo pipefail
 # Record where this script was called from so we can cd back to it anytime
 script_dir=$(pwd)
 
-# PartDB requires PHP 8.4 but the Bookworm installation does not list it as
+# PartDB requires PHP but the Bookworm installation does not list it as
 # a package so we download the package and install it manually
-sudo wget -qO /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+sudo curl -sSL https://packages.sury.org/php/README.txt | sudo bash -x
 
-# Update the package registry
-sudo apt update
+# Update the package registry and upgrade
+sudo apt update && sudo apt upgrade -y
 
 # For the installation of Part-DB, we need some prerequisites
 sudo apt install -y git curl zip ca-certificates software-properties-common apt-transport-https lsb-release nano wget
@@ -52,7 +51,7 @@ curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /us
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 # Install yarn
-sudo apt install -y yarn
+sudo apt update && sudo apt install -y yarn
 
 # Download Part-DB into the new folder /flamingonet/www/partdb
 # Must use mv here so the current user is the owner of the repo
@@ -65,10 +64,12 @@ sudo mv partdb /flamingonet/www
 cd /flamingonet/www/partdb
 
 # Make a copy of the .env file so we can configure Part-DB how we want it
-sudo -u $USER cp "$script_dir/../partdb/.env" .env.local
+# Set the owner to be www-data which is the apache user
+sudo cp "$script_dir/../partdb/.env" .env.local
+sudo chown -R www-data:www-data .
 
 # Install composer dependencies
-composer install --no-dev -o
+sudo -u www-data composer install --no-dev -o
 
 # Install yarn dependencies
 yarn install -y
@@ -88,29 +89,42 @@ sudo apt install -y mariadb-server
 # Configure Maria DB
 # Follow the directions here to get through this part because it is interactive
 # https://docs.part-db.de/installation/installation_guide-debian.html#mysqlmariadb-database
-echo "Running mysql_secure_installation..."
-echo -e "Please refer to the instructions here: https://docs.part-db.de/installation/installation_guide-debian.html#mysqlmariadb-database\n"
-sudo mysql_secure_installation
+echo -e "\nn\nn\ny\ny\ny\ny\n" | sudo mysql_secure_installation
+
+# Then create the database and user
+echo "CREATE DATABASE partdb;" | sudo mariadb || true
+echo "GRANT ALL PRIVILEGES ON partdb.* TO 'partdb'@'127.0.0.1' IDENTIFIED BY 'Mypartdbaccount123^';" | sudo mariadb || true
+echo "FLUSH PRIVILEGES;" | sudo mariadb || true
 
 # Update the database schema with the following command
-php bin/console doctrine:migrations:migrate
+# Record the initial password
+initial_password=$(sudo -u www-data php bin/console doctrine:migrations:migrate | grep "The initial password for the \"admin\" user is:")
 
-# Copy the partdb.conf file to the sites-available directory to make it available for Apache
-cd "$script_dir"
-sudo -u $USER cp ../partdb/partdb.conf /etc/apache2/sites-available/partdb.conf
+# Putting this in brackets to catch any errors and still output the initial password
+{
 
-# Activate the new site by
-sudo -u $USER ln -s /etc/apache2/sites-available/partdb.conf /etc/apache2/sites-enabled/partdb.conf
+  # Copy the partdb.conf file to the sites-available directory to make it available for Apache
+  cd "$script_dir"
+  sudo cp -p ../partdb/partdb.conf /etc/apache2/sites-available/partdb.conf
+  sudo ln -s /etc/apache2/sites-available/partdb.conf /etc/apache2/sites-enabled/partdb.conf
+  sudo a2enmod rewrite
 
-# Configure Apache to show pretty URL paths for Part-DB (/label/dialog instead of /index.php/label/dialog):
-sudo a2enmod rewrite
+  # (Re)start the apache2 webserver with
+  sudo service apache2 restart
 
-# (Re)start the apache2 webserver with
-sudo service apache2 restart
+  # Enable SSL for Part-DB
+  # See last chapter of this video for SSL: https://www.youtube.com/watch?v=VXSgEvZKp-8
+  # echo "Enabling SSL for Part-DB..."
+  # echo -e "Please refer to the instructions here: https://www.youtube.com/watch?v=VXSgEvZKp-8\n"
+  # sudo apt install -y certbot python3-certbot-apache
+  # sudo certbot --apache -d flamingonet.io
 
-# Enable SSL for Part-DB
-# See last chapter of this video for SSL: https://www.youtube.com/watch?v=VXSgEvZKp-8
-echo "Enabling SSL for Part-DB..."
-echo -e "Please refer to the instructions here: https://www.youtube.com/watch?v=VXSgEvZKp-8\n"
-sudo apt install -y certbot python3-certbot-apache
-sudo certbot --apache -d flamingonet.io
+  echo -e "\nSetup is complete!"
+  echo $initial_password
+
+} || {
+
+  echo -e "\nAn error occured while setting up Part-DB, but the Database was created successfully."
+  echo $initial_password
+
+}
